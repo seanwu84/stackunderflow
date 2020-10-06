f0e082337c21abc06c94858066cdaa6378b6959a
seanwu84 <seanwu84@gmail.com>
2020-10-06 18:46:14 -0400

    create users-signup api

---
+++ routes/api/users-signup.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const router = express.Router();
+//const {routeHandler, handleValidationErrors} = require("../utils"); TBD
+
+const bcrypt = require('bcryptjs');
+const {}
