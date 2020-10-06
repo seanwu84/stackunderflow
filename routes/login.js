44d2ed390774272435f1e102a5c0e724f99b6426
bacbartlett <brandonacb@gmail.com>
2020-10-06 12:57:39 -0400

    Moved login api to api route

--- routes/login.js
+++ routes/login.js
@@ -9,14 +9,5 @@ router.get("/", (req, res, next) =>{
     res.render("login")
 });
 
-router.post("/", checkLoginDetails, (req, res, next) =>{
-    res.json({token: req.newToken})
-});
-
-//this sends back the username
-router.get("/test-token", checkToken, (req, res)=>{
-    res.send(req.user.username)
-})
-
 
 module.exports = router;
