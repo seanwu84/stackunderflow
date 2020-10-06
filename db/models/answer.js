a1aa3cb6e1a8d0e92e1443ac4bca33f13b6ff0ac
Harrison Higgins <harryrhiggins@gmail.com>
2020-10-06 14:59:26 -0600

    Added questionId to Answers and fixed the down of the previous answer migration.

--- db/models/answer.js
+++ db/models/answer.js
@@ -4,12 +4,23 @@ module.exports = (sequelize, DataTypes) => {
     content: {
       type: DataTypes.TEXT,
       allowNull: false
-    }
+    },
+    userId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: { model: "Users" },
+    },
+    questionId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: { model: "Questions" },
+    },
   }, {});
   Answer.associate = function(models) {
-    Answer.hasMany(models.AnswerVote, { foreignKey: 'answerId'});
-    Answer.hasMany(models.AnswerComment, { foreignKey: "answerId" });
     Answer.belongsTo(models.User, { foreignKey: "userId" });
+    Answer.belongsTo(models.Quesion, { foreignKey: "questionId" });
+    Answer.hasMany(models.AnswerComment, { foreignKey: "answerId" });
+    Answer.hasMany(models.AnswerVote, { foreignKey: 'answerId'});
   };
   return Answer;
 };
