90530ca86abf45e6f032a95d43d27a5fb884e1f8
bacbartlett <brandonacb@gmail.com>
2020-10-07 19:35:47 -0400

    Fixed typo after testing

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ const router = express.Router();
 
 router.use("/api", apiRouter);
 
-router.use("/user", userRouter);
+router.use("/users", userRouter);
 
 router.get("/", (req, res) => {
   res.render("index");
