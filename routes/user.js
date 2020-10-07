c030175702627eac196462cb0256ad16eec03a9d
bacbartlett <brandonacb@gmail.com>
2020-10-07 19:33:05 -0400

    Cleaned up routers

--- routes/user.js
+++ routes/user.js
@@ -5,13 +5,17 @@ const {checkToken, checkLoginDetails, generateNewToken} = require("../utils/auth
 const router = express.Router();
 
 
-router.get("/", checkToken, (req, res, next) =>{
+router.get("/login", checkToken, (req, res, next) =>{
     if(req.user){
         res.redirect("/");
         return;
     }
     res.render("login");
 });
+
+router.get("/sign-up", (req, res) => {
+    res.render("sign-up");
+  });
 
 
 module.exports = router;
