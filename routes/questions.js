566f95b8edb82a75c785b248d903b2a1115d52d9
Harrison Higgins <harryrhiggins@gmail.com>
2020-10-08 08:38:52 -0600

    commit so that I can rebase on master

--- routes/questions.js
+++ routes/questions.js
@@ -1,7 +1,7 @@
 const express = require('express');
 
 const { asyncHandler } = require('../utils/utils');
-const { Question, Answer, QuestionVote, AnswerVote } = require('../db/models');
+const { Question, Answer, QuestionVote, AnswerVote, sequelize } = require('../db/models');
 
 const router = express.Router();
 
@@ -10,7 +10,13 @@ const countVotes = (votes) => {
 }
 
 router.get('/', asyncHandler(async (req, res, next) => {
-  const questions = await Question.findAll();
+  const questions = await sequelize.query("SELECT ")
+  const questions = await Question.findAll({
+    include: [{
+      model: QuestionVote,
+      
+    }]
+  });
   console.log(questions);
   res.send("HEllo");
 }));
@@ -18,38 +24,40 @@ router.get('/', asyncHandler(async (req, res, next) => {
 router.get('/:id(\\d+)', asyncHandler(async (req, res, next) => {
   const id = parseInt(req.params.id, 10);
 
-  const question = await Question.findOne({
-    where: id,
-    include: [{
-      model: User
-    }, {
-      model: Answer,
-      include: [{ model: AnswerVote }]
-    }, {
-      model: QuestionVote
-    }]
-  });
+  // const question = await Question.findOne({
+  //   where: id,
+  //   include: [{
+  //     model: User
+  //   }, {
+  //     model: Answer,
+  //     limit: 50,
+  //     order: []
+  //     include: [{ model: AnswerVote }]
+  //   }, {
+  //     model: QuestionVote
+  //   }]
+  // });
 
   // console.log(question);
-  console.log(question.User);
-  console.log(question.QuestionVotes);
-  const questionScore = countVotes(question.QuestionVotes);
-  console.log(questionScore);
-  questionData = {
-    id: question.id,
-    title: question.title,
-    content: question.content,
-    posted: question.createdAt,
-    votes: questionScore,
-    user: question.User
-  }
-  answerData = question.Answers.map(answer => {
-    {
-      id: answer.id,
+  // console.log(question.User);
+  // console.log(question.QuestionVotes);
+  // const questionScore = countVotes(question.QuestionVotes);
+  // console.log(questionScore);
+  // questionData = {
+  //   id: question.id,
+  //   title: question.title,
+  //   content: question.content,
+  //   posted: question.createdAt,
+  //   votes: questionScore,
+  //   user: question.User
+  // }
+  // answerData = question.Answers.map(answer => {
+  //   {
+  //     id: answer.id,
 
-    }
-  })
-  }
+  //   }
+  // })
+  // }
   res.render('question', {
     questionData
   });
