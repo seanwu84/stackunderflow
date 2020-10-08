0f5c73bf4493e2f6309004b758861edcd57ddb1a
bacbartlett <brandonacb@gmail.com>
2020-10-08 11:32:19 -0400

    Login screen styled. Includes error message styling

--- public/js/login.js
+++ public/js/login.js
@@ -1,4 +1,5 @@
 const submitForm = async (e) =>{
+    document.getElementById("errorDiv").innerHTML = ""
     e.preventDefault() 
     const form = e.target;
     const data = new FormData(form);
@@ -16,12 +17,25 @@ const submitForm = async (e) =>{
         window.location.href = "/";
     } else{
         const errors = JSON.parse(resJSON).messages;
-        //render errors
+        errors.forEach(function(el) {
+            const errorMessageDisplay = document.createElement("p");
+            errorMessageDisplay.innerHTML = el;
+            errorMessageDisplay.classList.add("errorMessage")
+            document.getElementById("errorDiv").appendChild(errorMessageDisplay)
+        })
     }
     
+}
+
+const demoLogin = (e) =>{
+    e.stopPropagation();
+    e.preventDefault();
+    document.getElementById("email").value = "jamesballard@gmail.com"
+    document.getElementById("password").value = "slambam123"
 }
 
 
 window.addEventListener("DOMContentLoaded", ()=>{
     document.getElementById("login-form").addEventListener("submit", submitForm)
+    document.getElementById("demoLogin").addEventListener("click", demoLogin)
 })
