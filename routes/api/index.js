97d8bdfbffbe29cf1378e0b5e8c322e81091c561
bacbartlett <brandonacb@gmail.com>
2020-10-08 13:45:34 -0400

    Need to pull

--- routes/api/index.js
+++ routes/api/index.js
@@ -11,6 +11,6 @@ const router = express.Router();
 router.use(verifyUser);
 router.use("/users", apiUsersRouter);
 router.use("/questions", questionsRouter);
-router.use("./search", apiSearchRouter);
+router.use("/search", apiSearchRouter);
 
 module.exports = router;
--- routes/api/search.js
+++ routes/api/search.js
@@ -1,10 +1,24 @@
 const express = require("express");
-
+const {Question, Answer} = require("../../db/models");
+const {Op} = require("sequelize")
+const {asyncHandler} = require("../../utils/utils");
 const router = express.Router();
 
-router.get("/", (req, res) =>{
-    res.json(JSON.stringify({message: "yay!"}))
-});
+router.post("/:sortType", asyncHandler( async(req, res, next) =>{
+    console.log("Got here")
+    console.log(req.body)
+    const {searchTerm} = req.body;
+    const questions = await Question.findAll({
+        limit: 10,
+        where: {
+            title: {
+                [Op.like]: `%${searchTerm}%`
+            }
+        }
+    });
+    res.json(questions)
+
+}));
 
 
 module.exports = router;
