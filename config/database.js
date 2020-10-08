da6a438989fdae27fc72e949006ac417765439c8
seanwu84 <seanwu84@gmail.com>
2020-10-08 12:51:52 -0400

    heroku commit

--- config/database.js
+++ config/database.js
@@ -14,5 +14,10 @@ module.exports = {
     host,
     dialect: "postgres",
     seederStorage: "sequelize"
+  },
+    production: {
+    use_env_variable: 'DATABASE_URL',
+    dialect: 'postgres',
+    seederStorage: 'sequelize',
   }
 };
