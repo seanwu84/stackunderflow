d891db4648674bfc5701acf473dd56b157bb3e20
Jessica Abels <jessabels92@gmail.com>
2020-10-08 17:01:06 -0700

    Created fetch request for question comments and created handlebars template

--- public/js/comments.js
+++ public/js/comments.js
@@ -1,3 +1,32 @@
+// import { render } from "/js/utils.js"
+
+document.addEventListener("DOMContentLoaded", async ()=> {
+    const question = document.querySelector(".question")    
+    const questionId = question.dataset.questionId
+
+    const res = await fetch(`/api/questions/${questionId}/comments`);
+    const comments = await res.json();
+    console.log(comments)
+    const templateRes = await fetch("/templates/comments.hb");
+    const template = await templateRes.text();
+
+    const compiledTemplate = Handlebars.compile(template);
+    const commentsHtml = compiledTemplate({
+        comments: comments.questionComments
+    })
+
+    // document.querySelector(".question .comments").innerHTML = commentsHtml;
+
+    // render()
+})
+
+
+
+
+
+
+
+
 // import { handleErrors } from "../../utils/utils";
 
 // const form = document.querySelector(".create-comment-form");
