ac9751fcba242d0fb8ff5942da1971b2d735b1c9
bacbartlett <brandonacb@gmail.com>
2020-10-10 16:23:37 -0400

    Fixed the router issue

--- routes/api/questions.js
+++ routes/api/questions.js
@@ -3,10 +3,10 @@ const { check } = require("express-validator");
 
 const { verifyUser } = require("../../utils/auth");
 const { handleValidationErrors, asyncHandler, csrfProtection } = require("../../utils/utils");
+const { User, Question, QuestionComment, AnswerComment } = require("../../db/models");
 const apiAnswersRouter = require("./answers");
+
 const router = express.Router();
-const db = require("../../db/models");
-const { User, Question, Answer, QuestionComment, AnswerComment, AnswerVote, QuestionVote, sequelize } = db;
 
 router.use('/:questionId(\\d+)/answers', apiAnswersRouter);
 
@@ -75,80 +75,6 @@ router.post(
   })
 );
 
-router.post(
-  "/:questionId(\\d+)/answers/:answerId(\\d+)/comment",
-  verifyUser,
-  validateComment,
-  asyncHandler(async (req, res) => {
-    const { content } = req.body;
-    const answerComment = await AnswerComment.create({
-      content,
-      userId: req.user.id,
-      answerId: req.params.answerId,
-    });
-    res.json({ answerComment });
-  })
-);
-
-// router.get(
-//   "/:questionId(\\d+)/votestotal",
-//   asyncHandler(async(req, res) => {
-//     const questionId = req.params.questionId;
-//     const question = await Question.findOne({
-//       where: {id:questionId },
-//       attributes: {
-//         include: [
-//           [
-//             sequelize.literal(`(
-//                     SELECT COALESCE(SUM(qv.value), 0)
-// 	                  FROM "QuestionVotes" AS qv
-// 	                  WHERE
-//                     qv."questionId" = "Question".id
-//                 )`),
-//             'score'
-//           ],
-//         ]
-//       },
-//     }
-//     );
-//     res.json({ score: question.get().score })
-//   })
-// )
-
-
-// router.get(
-//   "/:questionId(\\d+)/answers/:answerId(\\d)/votestotal",
-//   asyncHandler(async (req, res) => {
-//     const answerId = req.params.answerId;
-//     const answer = await Answer.findOne({
-//       where: { id: answerId },
-//       attributes: {
-//         include: [
-//           [
-//             sequelize.literal(`(
-//                     SELECT COALESCE(SUM(av.value), 0)
-// 	                  FROM "AnswerVotes" AS av
-// 	                  WHERE
-//                     av."answerId" = "Answer".id
-//                 )`),
-//             'score'
-//           ],
-//         ]
-//       },
-//     }
-//     );
-//     res.json({ score: answer.get().score })
-//   })
-// )
-
-
-//current state   upvote   downvote
-//1               0        -1
-//0               1        -1
-//-1              1         0
-//undef           create/1 create/-1
-
-
 router.post('/:questionId(\\d+)/vote', asyncHandler(async (req, res) => {
   const questionId = req.params.questionId;
   const { voteValue } = req.body;
@@ -181,8 +107,8 @@ router.post('/:questionId(\\d+)/vote', asyncHandler(async (req, res) => {
         [
           sequelize.literal(`(
                     SELECT COALESCE(SUM(qv.value), 0)
-	                  FROM "QuestionVotes" AS qv
-	                  WHERE
+                    FROM "QuestionVotes" AS qv
+                    WHERE
                     qv."questionId" = "Question".id
                 )`),
           'score'
@@ -193,52 +119,6 @@ router.post('/:questionId(\\d+)/vote', asyncHandler(async (req, res) => {
   );
 
   res.json({ currentVoteValue: question.dataValues.score, currentStateValue: currentState.value })
-}));
-
-
-
-router.post('/:questionId(\\d+)/answers/:answerId(\\d)/vote', asyncHandler(async (req, res) => {
-  const answerId = req.params.questionId;
-  const { voteValue } = req.body;
-  const currentState = await AnswerVote.findOne({
-    where: {
-      answerId: answerId,
-      userId: req.user.id
-    }
-  })
-  if (!currentState) {
-    await AnswerVote.create({
-      userId: req.userId.id,
-      value: voteValue,
-      answerId: req.params.answerId,
-    });
-  } else if (currentState.value === voteValue) {
-    currentState.value = 0;
-    await currentState.save()
-  }
-  else {
-    currentState.value = voteValue;
-    await currentState.save()
-  }
-  const answer = await Answer.findOne({
-    where: { id: answerId },
-    attributes: {
-      include: [
-        [
-          sequelize.literal(`(
-                    SELECT COALESCE(SUM(av.value), 0)
-	                  FROM "AnswerVotes" AS av
-	                  WHERE
-                    av."answerId" = "Answer".id
-                )`),
-          'score'
-        ],
-      ]
-    },
-  }
-  );
-
-  res.json({ currentVoteValue: answer.dataValues.score, currentStateValue: currentState.value })
 }));
 
 module.exports = router;
